3b7bd74890fbab26a28ab1aea738efc6791ca6dd
growinfrance <growinfrance@gmail.com>
2025-07-03 13:53:47 +0000

    Deploying to gh-pages from @ growinfrance/growinfrance.github.io@5a585ad4390b01a0177148e5a981825c0e9fba9f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-法国教育",
+  },{id: "nav-法国教育",
           title: "法国教育",
           description: "分享法国教育体系和我亲身经历的法国教育感悟",
           section: "Navigation",
           handler: () => {
             window.location.href = "/france-education/";
+          },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
